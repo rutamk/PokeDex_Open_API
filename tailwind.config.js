46949ce68c43734c057c728f77d2d2d54039f22b
rutamk <rutam2003@gmail.com>
2024-09-11 18:04:21 +0530

    sd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [],a
+}
